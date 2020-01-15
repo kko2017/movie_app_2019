bb45aee4ce5640fc6b9569158d47263b5f19c01b
KwangEun Oh <kwangeunoh1@gmail.com>
2020-01-14 21:53:17 -0400

    Map Recap

--- src/App.js
+++ src/App.js
@@ -1,20 +1,39 @@
 import React from "react";
 
-function Food({ fav }) {
-  console.log({ fav });
-  return <h1>I Like {fav}</h1>;
+// function Food({ fav }) {
+//   console.log({ fav });
+//   return <h1>I Like {fav}</h1>;
+// }
+
+function Food({ name }) {
+  return <h1>I Like {name}</h1>;
+}
+
+const foodILike = [
+  { id: 1, name: "ramen" },
+  { id: 2, name: "kimchi" },
+  { id: 3, name: "Orange" }
+];
+
+function renderDish(dish) {
+  console.log(dish);
+  return <Food name={dish.name} />;
 }
 
 function App() {
   return (
-    <div className="App">
-      <h3>Hello!</h3>
-      <Food fav="Nangmyun" />
-      <Food fav="Ramen" />
-      <Food fav="Kimchi" />
-      <Food fav="Sogogi" />
-      <Food fav="Bibimbab" />
-    </div>
+    <div>{foodILike.map(renderDish)}</div>
+    // <div className="App">
+    //   {foodILike.map(dish => (
+    //     <Food key={dish.id} name={dish.name} />
+    //   ))}
+
+    //   {/* <Food fav="Nangmyun" />
+    //   <Food fav="Ramen" />
+    //   <Food fav="Kimchi" />
+    //   <Food fav="Sogogi" />
+    //   <Food fav="Bibimbab" /> */}
+    // </div>
   );
 }
 
