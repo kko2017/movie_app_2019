66c6f7e54255ae86fa9ca7c757405649e37907d3
KwangEun Oh <kwangeunoh1@gmail.com>
2020-01-14 23:11:34 -0400

    PropTypes

--- src/App.js
+++ src/App.js
@@ -1,39 +1,50 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 // function Food({ fav }) {
 //   console.log({ fav });
 //   return <h1>I Like {fav}</h1>;
 // }
 
-function Food({ name }) {
-  return <h1>I Like {name}</h1>;
+function Food({ name, rating }) {
+  return (
+    <div>
+      <h1>I Like {name}</h1>
+      <h3>Rating: {rating}</h3>
+    </div>
+  );
 }
+
+Food.propTypes = {
+  name: PropTypes.string.isRequired,
+  rating: PropTypes.number.isRequired
+};
 
 const foodILike = [
-  { id: 1, name: "ramen" },
-  { id: 2, name: "kimchi" },
-  { id: 3, name: "Orange" }
+  { id: 1, name: "ramen", rating: 4.5 },
+  { id: 2, name: "kimchi", rating: 3.5 },
+  { id: 3, name: "Orange", rating: 5.0 }
 ];
 
-function renderDish(dish) {
-  console.log(dish);
-  return <Food name={dish.name} />;
-}
+// function renderDish(dish) {
+//   console.log(dish);
+//   return <Food name={dish.name} />;
+// }
 
 function App() {
   return (
-    <div>{foodILike.map(renderDish)}</div>
-    // <div className="App">
-    //   {foodILike.map(dish => (
-    //     <Food key={dish.id} name={dish.name} />
-    //   ))}
+    // <div>{foodILike.map(renderDish)}</div>
+    <div className="App">
+      {foodILike.map(dish => (
+        <Food key={dish.id} name={dish.name} rating={dish.rating} />
+      ))}
 
-    //   {/* <Food fav="Nangmyun" />
-    //   <Food fav="Ramen" />
-    //   <Food fav="Kimchi" />
-    //   <Food fav="Sogogi" />
-    //   <Food fav="Bibimbab" /> */}
-    // </div>
+      {/* <Food fav="Nangmyun" />
+      <Food fav="Ramen" />
+      <Food fav="Kimchi" />
+      <Food fav="Sogogi" />
+      <Food fav="Bibimbab" /> */}
+    </div>
   );
 }
 
