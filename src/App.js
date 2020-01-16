60146d821268c54ac2ca2941bbb255f3c48b4490
KwangEun Oh <kwangeunoh1@gmail.com>
2020-01-16 19:27:35 -0400

    Planning Movie Components

--- src/App.js
+++ src/App.js
@@ -1,47 +1,19 @@
 import React from "react";
 
 class App extends React.Component {
-  constructor(prop) {
-    super(prop);
-    console.log("I am constructor.");
-  }
-
-  // You don't have to change the state directly.
-  // State is data that is able to change itself in the indirect way.
   state = {
-    number: 0
-  };
-
-  // You must use the setState method should you want to change the state.
-  // If you change data using <this.state>, render function is not going to be refreshed.
-  add = () => {
-    console.log("Add");
-    this.setState(current => ({ number: current.number + 1 }));
-  };
-
-  minus = () => {
-    console.log("Minus");
-    this.setState(current => ({ number: current.number - 1 }));
+    isLoading: true
   };
 
   componentDidMount() {
-    console.log("Component rendered.");
-  }
-
-  componentDidUpdate() {
-    console.log("I just updated.");
+    setTimeout(() => {
+      this.setState({ isLoading: false });
+    }, 6000);
   }
 
   render() {
-    // return <h1>I am a class component.</h1>;
-    console.log("Component is redering now.");
-    return (
-      <div>
-        <h1>This number is {this.state.number}</h1>
-        <button onClick={this.add}>Add</button>
-        <button onClick={this.minus}>Minus</button>
-      </div>
-    );
+    const { isLoading } = this.state;
+    return <div>{isLoading ? "Loading..." : "We are ready."}</div>;
   }
 }
 
