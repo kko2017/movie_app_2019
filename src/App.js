4907c401653bf72e2641919a3e234ccfd9999cc3
KwangEun Oh <kwangeunoh1@gmail.com>
2020-01-15 20:48:10 -0400

    Class Component, State, and SetState

--- src/App.js
+++ src/App.js
@@ -1,51 +1,34 @@
 import React from "react";
-import PropTypes from "prop-types";
 
-// function Food({ fav }) {
-//   console.log({ fav });
-//   return <h1>I Like {fav}</h1>;
-// }
+class App extends React.Component {
+  // You don't have to change the state directly.
+  // State is data that is able to change itself in the indirect way.
+  state = {
+    number: 0
+  };
 
-function Food({ name, rating }) {
-  return (
-    <div>
-      <h1>I Like {name}</h1>
-      <h3>Rating: {rating}</h3>
-    </div>
-  );
-}
+  // You must use the setState method should you want to change the state.
+  // If you change data using <this.state>, render function is not going to be refreshed.
+  add = () => {
+    console.log("Add");
+    this.setState(current => ({ number: current.number + 1 }));
+  };
 
-Food.propTypes = {
-  name: PropTypes.string.isRequired,
-  rating: PropTypes.number.isRequired
-};
+  minus = () => {
+    console.log("Minus");
+    this.setState(current => ({ number: current.number - 1 }));
+  };
 
-const foodILike = [
-  { id: 1, name: "ramen", rating: 4.5 },
-  { id: 2, name: "kimchi", rating: 3.5 },
-  { id: 3, name: "Orange", rating: 5.0 }
-];
-
-// function renderDish(dish) {
-//   console.log(dish);
-//   return <Food name={dish.name} />;
-// }
-
-function App() {
-  return (
-    // <div>{foodILike.map(renderDish)}</div>
-    <div className="App">
-      {foodILike.map(dish => (
-        <Food key={dish.id} name={dish.name} rating={dish.rating} />
-      ))}
-
-      {/* <Food fav="Nangmyun" />
-      <Food fav="Ramen" />
-      <Food fav="Kimchi" />
-      <Food fav="Sogogi" />
-      <Food fav="Bibimbab" /> */}
-    </div>
-  );
+  render() {
+    // return <h1>I am a class component.</h1>;
+    return (
+      <div>
+        <h1>This number is {this.state.number}</h1>
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.minus}>Minus</button>
+      </div>
+    );
+  }
 }
 
 export default App;
