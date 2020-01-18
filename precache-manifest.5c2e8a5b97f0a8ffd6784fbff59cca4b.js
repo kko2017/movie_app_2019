1557759a27dde510e7eff67a2317736be35431f9
KwangEun Oh <kwangeunoh1@gmail.com>
2020-01-17 20:49:53 -0400

    Updates

---
+++ precache-manifest.5c2e8a5b97f0a8ffd6784fbff59cca4b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c0460e1f3ea347098b720d0ec24caf9",
+    "url": "/movie_app_2019/index.html"
+  },
+  {
+    "revision": "b7b5095c9a571c64706e",
+    "url": "/movie_app_2019/static/css/main.5721af45.chunk.css"
+  },
+  {
+    "revision": "ee671eb0fdd6ac954af0",
+    "url": "/movie_app_2019/static/js/2.a5dd137c.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/movie_app_2019/static/js/2.a5dd137c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b7b5095c9a571c64706e",
+    "url": "/movie_app_2019/static/js/main.4e76ec83.chunk.js"
+  },
+  {
+    "revision": "9df829dc075c0b9cff49",
+    "url": "/movie_app_2019/static/js/runtime-main.b175f17b.js"
+  }
+]);
